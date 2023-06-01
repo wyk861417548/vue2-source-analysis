31e71cc50e3eb8ef9df6485febda638cdd11d602
e <861417548@qq.com>
2023-06-01 11:27:58 +0800

    （6-1）数据劫持

--- src/core/instance/index.js
+++ src/core/instance/index.js
@@ -1,1 +1,11 @@
-console.log('初始化');
+import { initMinx } from "./init";
+
+// 不使用class 去创建类 是为了避免所有的方法耦合在一起
+function Vue(options){
+  this._init(options)
+}
+
+// 扩展 init 方法
+initMinx(Vue)
+
+export default Vue;
